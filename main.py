4bec09a803ae44076627887cfcac653784163595
서용덕 Yongduek Seo <yongduek.seo@gmail.com>
2020-02-28 17:11:36 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+    name:
+    class: gkscom
+"""
+
+def myFunction():
+    """ your code goes here """
+    pass
+    
+    
+if __name__ == "__main__":
+    print("main():")
+    myFunction()
